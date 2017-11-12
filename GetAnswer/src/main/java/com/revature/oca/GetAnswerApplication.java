1041a1954a00f260b87a17942c12e5b4af7c6c5c
Allen Arce <a.jarce@yahoo.com>
2017-11-12 16:09:58 -0500

    adding GetAnswer

---
+++ GetAnswer/src/main/java/com/revature/oca/GetAnswerApplication.java
@@ -0,0 +1,14 @@
+package com.revature.oca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GetAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAnswerApplication.class, args);
+	}
+}
---
+++ GetAnswer/src/main/java/com/revature/oca/web/GetCorrectAnswer.java
@@ -0,0 +1,27 @@
+package com.revature.oca.web;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.oca.bean.OcaQuestion;
+import com.revature.oca.dao.OcaQuestionRepository;
+
+@RestController
+public class GetCorrectAnswer {
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	OcaQuestionRepository ocaUserRepository;
+
+	@Transactional(readOnly = true)
+	@PostMapping("/getAnswer")
+	public OcaQuestion getAnswer(@RequestBody OcaQuestion ocaQuestion) {
+		return ocaQuestionRepository.findOne(ocaQuestion.getQuestionId());	//I have no idea what this is or where I access it
+	}
+}
